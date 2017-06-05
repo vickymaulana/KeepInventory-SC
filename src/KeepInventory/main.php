b3d15355ade08a939c3d752fedeb03ced51e19fd
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-06-05 21:58:42 +0700

    not finished

--- src/KeepInventory/main.php
+++ src/KeepInventory/main.php
@@ -12,3 +12,14 @@ use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
 
 class Main extends PluginBase implements Listener{
+  
+  public $drops = array();
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(textFormat::RED."KeepInventory.");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);}
+  
+  public function PlayerDeathPlayerDeathEvent $event){
+    $player = $event->getEntity();
+    $this->drops[$player->getName()][1] = $player-getInventory()->getArmorContents();
+    
