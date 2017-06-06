ad5365cd7c0c794d93525549cdf53ed72203bf4d
Samy <samysamuel000@gmail.com>
2017-06-06 17:19:31 +0300

    Update main.php

--- src/KeepInventory/main.php
+++ src/KeepInventory/main.php
@@ -1,25 +1,42 @@
 <?php
-
 namespace KeepInventory;
-
 use pocketmine\event\Listener;
 use pocketmine\player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\level\Level;
 use pocketmine\block\Block;
+use pocketmine\server;
 use pocketmine\entity\Entity;
+use pocketmine\utils\TextFormat as C;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
-
 class Main extends PluginBase implements Listener{
   
   public $drops = array();
   
   public function onEnable(){
-    $this->getServer()->getLogger()->info(textFormat::RED."KeepInventory.");
+    $this->getServer()->getLogger()->info(C::GREEN."KeepInventory by StormCraftMCPE and SamyR0.");
     $this->getServer()->getPluginManager()->registerEvents($this, $this);}
   
-  public function PlayerDeathPlayerDeathEvent $event){
+  public function onDisable(){
+    $this->getLogger()->info(C::RED . "KeepInventory disabled!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);}
+
+  public function PlayerDeath(PlayerDeathEvent $event){
     $player = $event->getEntity();
-    $this->drops[$player->getName()][1] = $player-getInventory()->getArmorContents();
-    
+    $this->drops[$player->getName()][1] = $player->getInventory()->getArmorContents();
+    $this->drops[$player->getName()][0] = $player->getInventory()->getContents();
+    $event->setDrops(array());
+    $player->teleport($player-getLevel()->getSpawn());
+    }
+  
+  public function PlayerRespawn(PlayerRespawnEvent $event){
+    $player = $event->getPlayer();
+   if (isset($this->drops[$player->getName()])){
+     $player->getInventory()->setContents($this->Drops[$player->getName()][0]);
+     $player->getInventory()->setArmorContents($this->Drops[$player->getName()][1]);
+     unset($this->drops[$player->getName()]);
+     }
+    }
+  }
+   
