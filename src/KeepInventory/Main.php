77618a96d194b4ba92ea2997c5ea49a8f05efe18
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-07-08 22:30:53 +0700

    cleanup

--- src/KeepInventory/Main.php
+++ src/KeepInventory/Main.php
@@ -8,4 +8,18 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\utils\server;
 
-class Main
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(C::GREEN."KeepInventory Enable!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+  
+  public function onDisable(){
+    $this->getLogger()->info(C::RED."KeepInventory Disable");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+  
+  public function PlayerDeath(PlayerDeathEvent $event);{
+    $event->setKeepInventory(true);
+    }
+  }
