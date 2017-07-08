db556935705f06e87a722e11949d6958a0ad65cd
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-07-08 22:20:34 +0700

    cleanup

---
+++ src/KeepInventory/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KeepInventory;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerDeathEvent
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\server;
+
+class Main
